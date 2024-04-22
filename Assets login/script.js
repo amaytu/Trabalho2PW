c5b93a42d2066817f06a4d728e631b71b08d9c6f
juliawel <julia20.wel@hotmail.com>
2024-04-21 23:32:40 -0300

    commit

--- Assets login/script.js
+++ Assets login/script.js
@@ -1,14 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
     var form = document.getElementById("login");
+    var emailInput = document.getElementById("email");
     var usuarioInput = document.getElementById("usuario");
     var senhaInput = document.getElementById("senha");
 
     form.addEventListener("submit", function(event) {
         event.preventDefault();
 
+        var emailInput = emailInput.value.trim();
         var usuarioValue = usuarioInput.value.trim();
         var senhaValue = senhaInput.value.trim();
 
+        if (!validateEmail(emailInput)) {
+            alert("Por favor, insira um email válido.");
+            return;
+        }
+        
         if (usuarioValue.length < 5) {
             alert("O usuário deve ter pelo menos 5 caracteres.");
             return;
@@ -21,4 +28,25 @@ document.addEventListener("DOMContentLoaded", function() {
 
         form.submit();
     });
+
+    function validateEmail(email) {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }
+
+    let radioButtons = document.querySelectorAll('input[type="radio"]');
+
+    radioButtons.forEach((radioButton) => {
+        radioButton.addEventListener('click', () => {
+            if (radioButton.checked) {
+            radioButtons.forEach((otherRadioButton) => {
+                if (otherRadioButton!= radioButton) {
+                otherRadioButton.checked = false;
+                }
+            });
+            }
+        });
+    });
 });
+
+
